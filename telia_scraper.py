be02358aad87a59f58c9ef49c68e3c091bc8dc3b
DonatasNoreika <donoras@gmail.com>
2024-10-29 11:50:20 +0200

    Įdėtas Telia Apple telefonų scraper'is

---
+++ telia_scraper.py
@@ -0,0 +1,25 @@
+
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+
+with open("telia_iphone_telefonai.csv", 'w', encoding="UTF-8", newline="") as file:
+    csv_writer = csv.writer(file)
+    csv_writer.writerow(['Phone model', "Price per month", "Full price"])
+
+    r = requests.get("https://www.telia.lt/prekes/telefonai-ir-priedai/mobilieji-telefonai/apple")
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    blocks = soup.find_all(class_="mobiles-product-card card card__product card--anim js-product-compare-product")
+    for block in blocks:
+        phone_model = block.find(class_="mobiles-product-card__title js-open-product").get_text().strip()
+        price_per_month = block.find_all(class_="mobiles-product-card__price-marker")[0].get_text().strip()
+        price_per_month = price_per_month.replace(",", ".").replace(" €/mėn.", "")
+        price = block.find_all(class_="mobiles-product-card__price-marker")[1].get_text().strip()
+        price = price.replace(" ", "").replace(" €", "")
+        csv_writer.writerow([phone_model, price_per_month, price])
+        print(phone_model)
+        print(price_per_month)
+        print(price)
+        print("----------------------------------")
